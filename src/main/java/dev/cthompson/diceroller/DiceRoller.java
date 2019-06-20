98fc38444fa8cf978252347595baf66572d6f44a
PsykotikDragon <52022617+PsykotikDragon@users.noreply.github.com>
2019-06-19 21:39:17 -0700

    Update DiceRoller.java
    [line 22] Updated version number from 1.3.1 to 1.3.2 pending submitted change

--- src/main/java/dev/cthompson/diceroller/DiceRoller.java
+++ src/main/java/dev/cthompson/diceroller/DiceRoller.java
@@ -19,7 +19,7 @@ public class DiceRoller
 {
     public static final String MODID = "diceroller";
     public static final String NAME = "Dice Roller";
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
 
     
     @EventHandler
